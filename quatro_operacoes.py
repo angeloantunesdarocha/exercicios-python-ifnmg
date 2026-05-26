e515fcc218fb393a584faed370b4e3e121d502fd
Ângelo Antunes <angeloantunesdarocha@gmail.com>
2026-05-26 11:01:15 -0300

    Exercícios iniciais em Python

---
+++ quatro_operacoes.py
@@ -0,0 +1,12 @@
+# Dois valores/4 operações
+num1=int(input("Digite um número: "))
+num2=int(input("Digite outro número: "))
+soma=num1+num2
+print("A soma é: ",soma)
+sub=num1-num2
+print("A subtração é: ",sub)
+div=num1/num2
+print("A divisão é: ",div)
+mult=num1*num2
+print("A multiplicação é: ",mult)
+print("As quatro operações feita!\nSoma, Subtração\nMultiplicação e Divisão")
