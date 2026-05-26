bab7e1841c0e3ce7df4fde6be2b104dcc04fa921
Ângelo Antunes <angeloantunesdarocha@gmail.com>
2026-05-26 10:57:50 -0300

    Exercício média de notas

---
+++ media_notas.py
@@ -0,0 +1,10 @@
+# Exercício Média de nota!
+nota1=float(input("Digite a primeira nota: "))
+nota2=float(input("Digite a segunda nota: "))
+nota3=float(input("Digite a terceira nota: "))
+nota4=float(input("Digite a quarta nota: "))
+soma=nota1+nota2+nota3+nota4
+print("Total das notas é: ",soma)
+div=soma/4
+print("Sua média é: ",div)
+
