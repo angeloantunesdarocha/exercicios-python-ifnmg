3c879a6dcf966a996bacbdb2aa07afcdbbf7fe3a
Ângelo Antunes <angeloantunesdarocha@gmail.com>
2026-05-26 10:56:55 -0300

    Exercício valor por hora trabalhada

---
+++ valor_hora_trabalho.py
@@ -0,0 +1,5 @@
+nome=input("Qual seu nome? ")
+sal=float(input("Qual o seu salário líquido mensal? "))
+horas=float(input("Quantas horas você trabalha por mês? "))
+hor=sal/horas
+print(nome,"Você recebe R$",hor,"reais\npor hora de trabalho!")
