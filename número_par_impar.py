e2d0ea61130750c9f2a079eebd2821bd585950ad
Ângelo Antunes <angeloantunesdarocha@gmail.com>
2026-05-27 15:36:29 -0300

    número_par_impar

---
+++ número_par_impar.py
@@ -0,0 +1,11 @@
+print()
+num=int(input("Digite um número: "))
+result=num%2
+if result == 0:
+ print()
+ print("O número digitado (",num,") é par!")
+else:
+    print("O número digitado (",num,") é ímpar!")
+print()
+print("Fim do algorítmo!")
+print()
