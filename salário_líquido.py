203fac793c5c50a13a4538a8cdc67136c4f95d18
Edilson Nogueira Jr. <edilsonbim@hotmail.com.br>
2026-05-27 08:18:33 -0300

    Add income tax exemption for salaries below 5000
    adicionando regra de isenção de imposto de renda para salários abaixo de 5000

--- salário_líquido.py
+++ salário_líquido.py
@@ -4,6 +4,10 @@ print()
 ir=float(salar*0.11)
 inss=float(salar*0.08)
 sind=(salar*0.05)
-liq1=salar-ir-inss-sind
-print(nome,",Os valores descontados são:\n\nR$",ir,"(imposto de renda)\nR$",inss,"(INSS)\nR$",sind,"(Contribuição Sindical)\n\nRestou R$",liq1,"(de salário liquido!)")
-
+if salar >= 5000:
+    liq1=salar-ir-inss-sind
+    print(nome,",Os valores descontados são:\n\nR$",ir,"(imposto de renda)\nR$",inss,"(INSS)\nR$",sind,"(Contribuição Sindical)\n\nRestou R$",liq1,"(de salário liquido!)")
+else:
+    liq1=salar-inss-sind
+    print(nome,",Os valores descontados são:\n\nR$ 0 (imposto de renda isento)\nR$",inss,"(INSS)\nR$",sind,"(Contribuição Sindical)\n\nRestou R$",liq1,"(de salário liquido!)")
+    
